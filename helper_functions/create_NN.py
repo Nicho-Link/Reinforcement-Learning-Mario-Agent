97e432a4ba466cb734d21722256f95c7b42a5736
Nicho <nicholaselink@gmail.com>
2024-06-11 09:01:02 +0200

    update the Neural Network

--- helper_functions/create_NN.py
+++ helper_functions/create_NN.py
@@ -1,27 +1,16 @@
 from torch import nn
+import copy
 
-class SMBAgentNN():
+class SMBAgentNN(nn.Module):
     def __init__(self, state_shape, num_actions):
-        channels, height, width, frames = state_shape
+        super().__init__()
+        frames, height, width = state_shape
 
         if height != 240 or width != 256 or frames != 10:
             raise ValueError(f"Expecting state shape: (1, 240, 256, 10), got: {state_shape}")
         
-        self.online = self.build_cnn(channels * frames, num_actions)
-        self.target = self.build_cnn(channels * frames, num_actions)
-        self.target.load_state_dict(self.online.state_dict())
-        for param in self.target.parameters():
-            param.requires_grad = False
-
-    def forward(self, state, model):
-        if model == "online":
-            return self.online(state)
-        elif model == "target":
-            return self.target(state)
-
-    def build_cnn(self, channels, num_actions):
-        return nn.Sequential(
-            nn.Conv2d(in_channels=channels, out_channels=32, kernel_size=8, stride=4),
+        self.online = nn.Sequential(
+            nn.Conv2d(in_channels=frames, out_channels=32, kernel_size=8, stride=4),
             nn.ReLU(),
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2),
             nn.ReLU(),
@@ -32,10 +21,12 @@ class SMBAgentNN():
             nn.ReLU(),
             nn.Linear(512, num_actions),
         )
-    
-    def to(self, device):
-        self.online.to(device)
-        self.target.to(device)
+        self.target = copy.deepcopy(self.online)
+        for param in self.target.parameters():
+            param.requires_grad = False
 
-    def parameters(self):
-        return self.online.parameters()
+    def forward(self, state, model):
+        if model == "online":
+            return self.online(state)
+        elif model == "target":
+            return self.target(state)
